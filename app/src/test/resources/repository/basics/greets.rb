86488630b51252ad038a6d30fc1f89d1aa7a3047
Cédric Boufflers <cedric.boufflers@nordnet.fr>
2013-12-19 10:40:33 +0100

    Fixed some issues with mainClass and ruby scripts being in java path

--- app/src/test/resources/repository/basics/greets.rb
+++ app/src/test/resources/repository/basics/greets.rb
@@ -1,4 +1,1 @@
-"the answer to life the universe and everything is... " +
-    dbi.foo.text("select 42") + 
-    "... or maybe it is " +
-    d.a.to_s
+dbi.foo.text("select DATEADD('DAY', ?, NOW())", d.jours)
